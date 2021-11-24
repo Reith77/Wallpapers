1e165620cfc49bc7b0d6094838ba0efd3903110a
Tejas <tejas@Reiths-Friend-Chan.local>
2021-11-24 20:40:38 +0530

    Added 42 Wallpapers. Also Added Image Sizing Script

---
+++ Image Sizer/sizer.py
@@ -0,0 +1,21 @@
+# import required module
+from PIL import Image
+import os
+
+images_dir='../images'
+
+for filename in os.listdir(images_dir):
+    path = os.path.join(images_dir, filename)
+    if filename.startswith('.'):
+        print(f"[ⓧ] Ignoring: {filename} at {path}")
+        continue
+    if not filename.startswith('`'):
+        print(f"[*] Calculating image size for {filename.split('.')[0]} at {path[2:]}")
+        img = Image.open(path)
+        width = img.width
+        height = img.height
+        print(f'\t[-] Width = {width}')
+        print(f'\t[-] Height = {height}')
+        new_name = images_dir + f"/`{filename.split('.')[0]} - {width}x{height}.{filename.split('.')[1]}"
+        os.rename(path, new_name)
+        print(f"[✅] Renamed {filename.split('.')[0]} successfully")
